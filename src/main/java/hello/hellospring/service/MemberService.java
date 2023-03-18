d3b46883dc21f28d193a05b5c2e0cd4ed89fe186
udonggi <dongki1882@naver.com>
2023-03-18 22:55:20 +0900

    java code add bean

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -10,14 +10,13 @@ import java.util.List;
 import java.util.Optional;
 
 
-@Service
+
 public class MemberService {
 
 
 
     private final MemberRepository memberRepository;
 
-    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
